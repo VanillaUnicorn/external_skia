e4b32bc6f86289b5daa9773cd759c8227d7c4b82
Steve Kondik <shade@chemlab.org>
2013-12-29 22:29:20 -0800

    skia: NEON blit optimizations from Samsung
     * Improves performance across the board
    
    Change-Id: I3aa66f69140acf3992fbeb6346de4444195fdf92
    
    Conflicts:
    	src/core/SkBlitter_RGB16.cpp

--- src/core/SkBlitter_RGB16.cpp
+++ src/core/SkBlitter_RGB16.cpp
@@ -549,15 +549,33 @@ static uint32_t pmcolor_to_expand16(SkPMColor c) {
     return (g << 24) | (r << 13) | (b << 2);
 }
 
+extern "C" {
+void skia_androidopt_blend32_16_optimized(uint32_t src, unsigned scale, uint16_t **pdst, int *pcount) __attribute__((weak));
+}
+
+#ifdef NEON_BLIT_H
+extern "C" void blitH_NEON(uint16_t *dst, int count, uint32_t src_expand, unsigned int scale);
+#endif
+
 static inline void blend32_16_row(SkPMColor src, uint16_t dst[], int count) {
     SkASSERT(count > 0);
     uint32_t src_expand = pmcolor_to_expand16(src);
     unsigned scale = SkAlpha255To256(0xFF - SkGetPackedA32(src)) >> 3;
-    do {
+
+#ifdef NEON_BLIT_H
+    blitH_NEON(dst, count, src_expand, scale);
+#else
+    if (skia_androidopt_blend32_16_optimized) {
+        skia_androidopt_blend32_16_optimized(src_expand, scale, &dst, &count);
+    }
+
+    while (count > 0) {
         uint32_t dst_expand = SkExpand_rgb_16(*dst) * scale;
         *dst = SkCompact_rgb_16((src_expand + dst_expand) >> 5);
         dst += 1;
-    } while (--count != 0);
+        --count;
+    }
+#endif
 }
 
 void SkRGB16_Blitter::blitH(int x, int y, int width) {
@@ -569,6 +587,13 @@ void SkRGB16_Blitter::blitH(int x, int y, int width) {
     blend32_16_row(fSrcColor32, device, width);
 }
 
+#ifdef NEON_BLIT_ANTI_H
+extern "C" void blitAntiH_NEON(const SkAlpha* SK_RESTRICT antialias,
+                               uint16_t * SK_RESTRICT device,
+                               const int16_t* SK_RESTRICT runs,
+                               uint32_t srcExpanded, unsigned scale);
+#endif
+
 void SkRGB16_Blitter::blitAntiH(int x, int y,
                                 const SkAlpha* SK_RESTRICT antialias,
                                 const int16_t* SK_RESTRICT runs) {
@@ -576,6 +601,9 @@ void SkRGB16_Blitter::blitAntiH(int x, int y,
     uint32_t    srcExpanded = fExpandedRaw16;
     unsigned    scale = fScale;
 
+#ifdef NEON_BLIT_ANTI_H
+    blitAntiH_NEON(antialias, device, runs, srcExpanded, scale);
+#else
     // TODO: respect fDoDither
     for (;;) {
         int count = runs[0];
@@ -599,6 +627,7 @@ void SkRGB16_Blitter::blitAntiH(int x, int y,
         }
         device += count;
     }
+#endif
 }
 
 static inline void blend_8_pixels(U8CPU bw, uint16_t dst[], unsigned dst_scale,
